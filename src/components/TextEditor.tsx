f213f69424970d90134ae61151e5ea95fd8a955e
Bui Gábor <bui.gabor97@gmail.com>
2021-03-05 11:44:33 +0100

    Add redux to the party

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,11 +1,22 @@
+/** @jsxImportSource @emotion/react */
 import MDEditor from '@uiw/react-md-editor';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import textEditorStyles from './styles/textEditorStyles';
 
 export const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+  const editorRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        editorRef.current &&
+        event.target &&
+        editorRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
       setEditing(false);
     };
     document.addEventListener('click', listener, { capture: true });
@@ -16,18 +27,26 @@ export const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div>
-        <MDEditor />
+      <div css={textEditorStyles} ref={editorRef}>
+        <MDEditor
+          value={value}
+          onChange={(text) => {
+            setValue(text || '');
+          }}
+        />
       </div>
     );
   }
   return (
     <div
+      css={textEditorStyles}
       onClick={() => {
         setEditing(true);
       }}
     >
-      <MDEditor.Markdown source={'# Header'} />
+      <div className="card">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
---
+++ src/state/cell.ts
@@ -0,0 +1,7 @@
+export type CellTypes = 'code' | 'text' | 'draw';
+
+export interface Cell {
+  id: string;
+  type: CellTypes;
+  content: string;
+}
