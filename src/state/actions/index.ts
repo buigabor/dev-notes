0ed3b1dd9c0cdf59528712e6b92b30219b76cbac
Bui Gábor <bui.gabor97@gmail.com>
2021-03-06 17:49:56 +0100

    Add action interfaces for modifying cells

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,24 +1,25 @@
 import { ActionType } from '../action-types';
 import { CellTypes } from '../cell';
 
-interface MoveCellAction {
+export type Direction = 'up' | 'down';
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
-    direction: 'up' | 'down';
+    direction: Direction;
   };
 }
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: { id: string; content: string };
 }
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
-  payload: { id: string; type: CellTypes };
+  payload: { id: string | null; type: CellTypes };
 }
 
 export type Action =
