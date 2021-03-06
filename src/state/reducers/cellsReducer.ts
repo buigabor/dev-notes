0407078c42ce6849e9322cb36c6d4f06c5e9adb3
Bui Gábor <bui.gabor97@gmail.com>
2021-03-06 17:49:17 +0100

    Add reducer for modifying cells

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -16,22 +17,65 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action,
-): CellsState => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      return state;
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    default:
-      return state;
-  }
+const cellsReducer = produce(
+  (state: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL: {
+        const { id, content } = action.payload;
+        state.data[id].content = content;
+        return state;
+      }
+      case ActionType.DELETE_CELL: {
+        state.order = state.order.filter((id) => {
+          return id !== action.payload;
+        });
+        delete state.data[action.payload];
+        return state;
+      }
+      case ActionType.MOVE_CELL: {
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+        // if target index is out of bound
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return state;
+        }
+
+        // Swap the cells
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+
+        return state;
+      }
+      case ActionType.INSERT_CELL_BEFORE: {
+        const cell: Cell = {
+          id: randomId(),
+          type: action.payload.type,
+          content: '',
+        };
+
+        state.data[cell.id] = cell;
+
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        if (index < 0) {
+          state.order.push(cell.id);
+        } else {
+          state.order.splice(index, 0, cell.id);
+        }
+        return state;
+      }
+      default:
+        return state;
+    }
+  },
+);
+
+const randomId = () => {
+  var S4 = function () {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substr(2, 7);
+  };
+  return S4() + S4() + '-' + S4() + '-' + S4() + S4() + S4();
 };
 
-export default reducer;
+export default cellsReducer;
