6a4e5d4626c7f9d56f9ca8895c828e95b798b66e
Bui Gábor <bui.gabor97@gmail.com>
2021-04-03 23:45:35 +0200

    Add quiz skeleton

--- packages/client/src/components/Quiz/Quiz.tsx
+++ packages/client/src/components/Quiz/Quiz.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+
+const quizStyles = css`
+
+`
 
 export const Quiz = () => {
   return (
-    <div>
-
+    <div css={quizStyles}>
+      <div className="quiz-wrapper">
+        <div className="quiz-question">Question for quiz</div>
+        <div className="quiz-answers-wrapper">
+          <div className="quiz-answer">Answer 1</div>
+          <div className="quiz-answer">Answer 2</div>
+          <div className="quiz-answer">Answer 3</div>
+          <div className="quiz-answer">Answer 4</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
