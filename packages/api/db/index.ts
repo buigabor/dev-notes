d8aabbb9a253afb7ea2c6cb1d91531b94f2b85de
Bui Gábor <bui.gabor97@gmail.com>
2021-04-10 20:55:15 +0200

    Change ley config file

--- packages/api/db/index.ts
+++ packages/api/db/index.ts
@@ -4,20 +4,20 @@ require('dotenv').config();
 const postgres = require('postgres');
 // let sql = postgres();
 
-export function setPostgresDefaultsOnHeroku() {
-  if (process.env.DATABASE_URL) {
-    const { parse } = require('pg-connection-string');
+// export function setPostgresDefaultsOnHeroku() {
+//   if (process.env.DATABASE_URL) {
+//     const { parse } = require('pg-connection-string');
 
-    // Extract the connection information from the Heroku environment variable
-    const { host, database, user, password } = parse(process.env.DATABASE_URL);
+//     // Extract the connection information from the Heroku environment variable
+//     const { host, database, user, password } = parse(process.env.DATABASE_URL);
 
-    // Set standard environment variables
-    process.env.PGHOST = host;
-    process.env.PGDATABASE = database;
-    process.env.PGUSERNAME = user;
-    process.env.PGPASSWORD = password;
-  }
-};
+//     // Set standard environment variables
+//     process.env.PGHOST = host;
+//     process.env.PGDATABASE = database;
+//     process.env.PGUSERNAME = user;
+//     process.env.PGPASSWORD = password;
+//   }
+// };
 
 interface globalThis {
   [key: string]: any; // Add index signature
---
+++ packages/api/ley.config.js
@@ -0,0 +1,29 @@
+// const setPostgresDefaultsOnHeroku = require('./db/index.ts')
+// import { setPostgresDefaultsOnHeroku } from './db/index';
+
+// setPostgresDefaultsOnHeroku()
+
+function setPostgresDefaultsOnHeroku() {
+  if (process.env.DATABASE_URL) {
+    const { parse } = require('pg-connection-string');
+
+    // Extract the connection information from the Heroku environment variable
+    const { host, database, user, password } = parse(process.env.DATABASE_URL);
+
+    // Set standard environment variables
+    process.env.PGHOST = host;
+    process.env.PGDATABASE = database;
+    process.env.PGUSERNAME = user;
+    process.env.PGPASSWORD = password;
+  }
+};
+
+setPostgresDefaultsOnHeroku()
+
+const options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.ssl = { rejectUnauthorized: false };
+}
+
+module.exports = options;
