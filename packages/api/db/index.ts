b4baa7b129232634c6296372b9175e8c38cd1921
Bui Gábor <bui.gabor97@gmail.com>
2021-04-11 11:17:19 +0200

    Remove setPostgresDefaultsOnHeroku imports...

--- packages/api/db/index.ts
+++ packages/api/db/index.ts
@@ -1,7 +1,18 @@
 import camelcaseKeys from 'camelcase-keys';
 import postgres from 'postgres';
 
-const setPostgresDefaultsOnHeroku = require('./setPostgresDefaultsOnHeroku');
+function setPostgresDefaultsOnHeroku(){if (process.env.DATABASE_URL) {
+  const { parse } = require('pg-connection-string');
+
+  // Extract the connection information from the Heroku environment variable
+  const { host, database, user, password } = parse(process.env.DATABASE_URL);
+
+  // Set standard environment variables
+  process.env.PGHOST = host;
+  process.env.PGDATABASE = database;
+  process.env.PGUSERNAME = user;
+  process.env.PGPASSWORD = password;
+}}
 setPostgresDefaultsOnHeroku();
 require('dotenv').config();
 // let sql = postgres();
--- packages/api/ley.config.js
+++ packages/api/ley.config.js
@@ -1,6 +1,24 @@
-const setPostgresDefaultsOnHeroku = require('./db/setPostgresDefaultsOnHeroku');
+// const setPostgresDefaultsOnHeroku = require('./db/setPostgresDefaultsOnHeroku');
 
+// setPostgresDefaultsOnHeroku();
+
+
+function setPostgresDefaultsOnHeroku() {
+  if (process.env.DATABASE_URL) {
+    const { parse } = require('pg-connection-string');
+
+    // Extract the connection information from the Heroku environment variable
+    const { host, database, user, password } = parse(process.env.DATABASE_URL);
+
+    // Set standard environment variables
+    process.env.PGHOST = host;
+    process.env.PGDATABASE = database;
+    process.env.PGUSERNAME = user;
+    process.env.PGPASSWORD = password;
+  }
+}
 setPostgresDefaultsOnHeroku();
+
 const options = {};
 
 if (process.env.NODE_ENV === 'production') {
